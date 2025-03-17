2ce6b2e778c9496886450e27f1d0dc599a3a25c1
Mats Huesmann <huesmann.mats@gmail.com>
2025-03-17 15:25:54 +0100

    Add Debug3D and fix slider to mouse movement

--- Scenes/path_3d.gd
+++ Scenes/path_3d.gd
@@ -1,10 +1,11 @@
 extends Path3D
 
-const RAY_LENGTH = 1
+const RAY_LENGTH = 100
+const SPEED = 30
 
-func _ready() -> void:
-	self.curve.get_baked_points()
-	
+func absoluteClosestSort(a, b) -> bool:
+	return abs(a) - abs(b) < 0
+
 func _process(delta: float) -> void:
 	if Input.is_action_pressed('slider_move'):
 		var mousePos = get_viewport().get_mouse_position()
@@ -16,9 +17,18 @@ func _process(delta: float) -> void:
 		rayQuery.from = from
 		rayQuery.to = to
 		var result = space.intersect_ray(rayQuery)
-		print(result)
-		#var position: Vector3 = result.position
-		#position = self.to_local(position)
-		#var progress = self.curve.get_closest_offset(position)
-		#var follower: PathFollow3D = self.find_child("PathFollow3D")
-		#follower.progress = progress
+		if(!result.has("position")):
+			return
+		var position: Vector3 = result.position
+		position = self.to_local(position)
+		var progress = self.curve.get_closest_offset(position)
+		var follower: PathFollow3D = self.find_child("PathFollow3D")
+		var length = self.curve.get_baked_length()
+		var targets = [progress - length, progress, progress + length]
+		var distances = [targets[0] - follower.progress, targets[1] - follower.progress, targets[2] - follower.progress]
+		distances.sort_custom(absoluteClosestSort)
+		var direction = 1 if distances[0] > 0 else -1;
+		if(abs(distances[0]) < direction / length * SPEED):
+			return
+		follower.progress += direction / length * SPEED
+		
